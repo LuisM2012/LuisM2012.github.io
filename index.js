f41486ddb30991aeac88a4c5ff4aff5efc81d646
Luis Morales <81542969+LuisM2012@users.noreply.github.com>
2023-01-10 20:39:23 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Handle Background
+// Handle Background Wrapper
 const textCodes = ["0<br/>0<br/>1<br/>1<br/>0<br/>0<br/>1<br/>0",
                   "0<br/>0<br/>0<br/>1<br/>0<br/>1<br/>0<br/>1",
                   "0<br/>1<br/>1<br/>1<br/>1<br/>0<br/>1<br/>0",
@@ -9,7 +9,7 @@ const textCodes = ["0<br/>0<br/>1<br/>1<br/>0<br/>0<br/>1<br/>0",
                   "1<br/>1<br/>0<br/>0<br/>1<br/>1<br/>1<br/>1"];
 const backWrap = document.getElementById("back-wrap");
 backWrap.innerHTML = "";
-for (let i = 0; i < 150; i++)
+for (let i = 0; i < Math.ceil(screen.width/10.4); i++)
 {
   backWrap.innerHTML += `<h3 class="back-text${Math.ceil(Math.random()*4)}">${textCodes[Math.floor(Math.random()*8)]}</h3>`;
 }
@@ -39,4 +39,3 @@ function openNav()
   backWrap.classList.add("blur");
   body.classList.add("noOver");
 }
-
